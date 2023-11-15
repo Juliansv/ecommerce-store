9ccc440fef2fa2718a87aa9aa8b060305bb0a0a6
Juliansv <juliansv22@gmail.com>
2023-11-15 18:19:59 -0300

    remove unused log

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -12,9 +12,6 @@ interface GalleryProps {
 
 const Gallery: React.FC<GalleryProps> = ({ images }) => {
 
-    console.log("images: ", images);
-    
-	
     return (
         <Tab.Group as="div" className="flex flex-col-reverse">
             <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
