01bdd112d6bda02aa345e5facc34c0a0b2191770
Juliansv <juliansv22@gmail.com>
2023-11-28 22:23:00 -0300

    Remove unused imports. Change product list style

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,5 @@
-import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
 import getSliderImages from "@/actions/get-slider-images";
-import Billboard from "@/components/billboard";
 import ProductList from "@/components/product-list";
 import Slider from "@/components/slider";
 import Container from "@/components/ui/container";
@@ -19,7 +17,7 @@ const HomePage = async () => {
 				<Slider data={slides}/>
 				<div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
 					<ProductList title="Featured Products" items={products} />
-				S</div>
+				</div>
 			</div>
 		</Container>
 	);
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -14,7 +14,7 @@ const ProductList: React.FC<ProductListProps> = ({ title, items }) => {
 			{!items.length ? (
 				<NoResults />
 			) : (
-				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-4">
 					{items.map((product) => (
 						<ProductCard key={product.id} data={product} />
 					))}
