5b1ad254afeb81835fa743aee25889b46b4c9a85
Juliansv <juliansv22@gmail.com>
2024-04-27 18:15:28 +0200

    Update date in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
 		<footer className="bg-[#11005C] border-t">
 			<div className="mx-auto py-10">
 				<p className="text-center text-xs text-white">
-					&copy; 2023 La Chalita, All rights reserved
+					&copy; 2024 La Chalita, All rights reserved
 				</p>
 			</div>
 		</footer>
